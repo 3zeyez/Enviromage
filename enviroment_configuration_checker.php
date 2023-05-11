9b5c98230b2395863335e51f6531ebcb9dfe2b62
Ahmed Aziz ABBASSI <77698923+aziz-712@users.noreply.github.com>
2023-05-12 00:41:14 +0100

    Update enviroment_configuration_checker.php
    realpath_cache_ttl is in seconds not need to be converted

--- enviroment_configuration_checker.php
+++ enviroment_configuration_checker.php
@@ -7,7 +7,7 @@ function getConfigurations(): array
     // $memory_limit = return_bytes(ini_get('memory_limit'));
     $max_execution_time = ini_get('max_execution_time');
     $realpath_cache_size = return_bytes(ini_get('realpath_cache_size'));
-    $realpath_cache_ttl = return_bytes(ini_get('realpath_cache_ttl'));
+    $realpath_cache_ttl = ini_get('realpath_cache_ttl');
     $upload_max_filesize = return_bytes(ini_get('upload_max_filesize'));
     $post_max_size = return_bytes(ini_get('post_max_size'));
 
