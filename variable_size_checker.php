9b13221169357bed77bda7b96985dbaa17770caa
Ahmed Aziz ABBASSI <77698923+aziz-712@users.noreply.github.com>
2023-05-15 17:23:58 +0100

    I've added array size calc

--- variable_size_checker.php
+++ variable_size_checker.php
@@ -3,12 +3,16 @@
 /**
  * Get size of a variable
  *
+ * These are done:
  * null     = 0 bytes in the heap
  * bool     = 4 bytes in the heap
  * int      = 4 bytes in the heap
  * float    = 8 bytes in the heap
  * string   = each char is 1 byte in the heap
- * array    =
+ *
+ * array    = each key has a Zend string size
+ *            and each value has a size as a Zval variable
+ * To Do:
  * object   =
  * callable =
  * resource =
@@ -20,20 +24,26 @@
  *   the actual size of the variable
  */
 
-function get_var_size(mixed $var)
+function get_var_size(mixed $var): int
 {
     // as every var in php is a zval
     // a zval takes automaticly 128 bit
-    $size = 128 % 8; // these is in bytes
+    $size = 128 / 8; // these is in bytes
 
     $var_type = gettype($var);
 
-    if ($var_type == 'boolean' || $var_type == 'integer') {
+    if ($var_type === 'boolean' || $var_type === 'integer') {
         $size += 4;
-    } else if ($var_type == 'double') {
+    } else if ($var_type === 'double') {
         $size += 8;
-    } else if ($var_type == 'string') {
+    } else if ($var_type === 'string') {
         $size += strlen($var);
+    } else if ($var_type === 'array') {
+        foreach ($var as $key => $value):
+            $size += get_var_size($key);
+            $size += get_var_size($value);
+        endforeach;
+
     }
 
     return $size;
