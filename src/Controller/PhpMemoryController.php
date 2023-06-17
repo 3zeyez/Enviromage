9d704032a9957b0d6cc7a9590abd5e10c56cea2c
aziz-712 <a7med3bassi.400@gmail.com>
2023-06-17 18:12:02 +0100

    Calculate Modules' memory size in the disk.

--- src/Controller/PhpMemoryController.php
+++ src/Controller/PhpMemoryController.php
@@ -1,12 +1,12 @@
 <?php /** @noinspection PhpUnused */
+
 /** @noinspection PhpUndefinedFunctionInspection */
 /**
  * @file
  * Contains \Drupal\php_memory_readiness_checker\Controller\phpController.
  */
 
-
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace Drupal\php_memory_readiness_checker\Controller;
 
@@ -19,12 +19,11 @@ use Drupal\Core\Asset\AttachedAssetsInterface;
 use InvalidArgumentException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-
 /**
  * Defines phpController class.
  */
-class PhpMemoryController extends ControllerBase
-{
+class PhpMemoryController extends ControllerBase {
+
   /**
    * Display the markup.
    *
@@ -32,25 +31,33 @@ class PhpMemoryController extends ControllerBase
    *   Return markup array.
    */
   public function content(): array {
-    return [
-      '#theme' => 'php_memory_readiness_checker',
-      '#environment_configuration' => $this->get_environment_configuration(),
-    ];
+    //    return [
+    //      '#theme' => 'php_memory_readiness_checker',
+    //      '#environment_configuration' => $this->listModules(),
+    //    ];
+    //    return [
+    //      '#markup' => $this->t(implode(', ', $this->listModules())),
+    //    ];
+    echo $this->human_filesize($this->getModulesSize()) . "<br/>";
+    echo "<pre>";
+    //    print_r($this->listModules());
+    echo "</pre>";
+    return [];
   }
 
   /**
    * Converts a human-readable size representation to bytes.
    *
    * Accepts a string representing the size, such as "2M" for 2 megabytes.
-   * The supported size suffixes are "k" for kilobytes, "m" for megabytes, and "g" for gigabytes.
-   * The function returns the equivalent size in bytes as an integer.
+   * The supported size suffixes are "k" for kilobytes, "m" for megabytes, and
+   * "g" for gigabytes. The function returns the equivalent size in bytes as an
+   * integer.
    *
    * @param string $size The human-readable size string to convert.
    *
    * @return int The size in bytes as an integer.
    */
-  private function return_bytes(string $size): int
-  {
+  private function return_bytes(string $size): int {
     $last = strtolower($size[strlen($size) - 1]);
     $size = (int) substr($size, 0, -1);
 
@@ -65,7 +72,8 @@ class PhpMemoryController extends ControllerBase
         case 'k':
           $size *= 1024;
       }
-    } else {
+    }
+    else {
       // On non-Windows systems, use decimal (1000) base for size conversion.
       switch ($last) {
         case 'g':
@@ -74,30 +82,65 @@ class PhpMemoryController extends ControllerBase
           $size *= 1000;
         case 'k':
           $size *= 1000;
-        }
+      }
     }
 
     return $size;
   }
 
+  private function human_filesize($bytes, $decimals = 2) {
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+      $factor = 1024;
+    } else {
+      $factor = 1000;
+    }
+
+    $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+    for ($i = 0; $bytes >= $factor && $i < count($units) - 1; $i++) {
+      $bytes /= $factor;
+    }
+
+    return round($bytes, 2) . ' ' . $units[$i];
+  }
+
+  private function getDirectorySize($directory) {
+    $total_size = 0;
+    $files = scandir($directory);
+    foreach ($files as $file) {
+      if ($file != "." && $file != "..") {
+        if (is_dir($directory . "/" . $file)) {
+          $total_size += $this->getDirectorySize($directory . "/" . $file);
+        }
+        else {
+          $total_size += filesize($directory . "/" . $file);
+        }
+      }
+    }
+    return $total_size;
+  }
+
   /**
    * Check memory readiness based on the specified memory limit.
    *
-   * This function checks if the available memory is sufficient based on the provided memory limit.
-   * If the memory limit is set to -1, indicating no limit, the function considers the memory as ready.
-   * Otherwise, it calculates the available memory by subtracting the current memory usage from the memory limit.
-   * If the available memory is below the threshold of 200MB, an error message is returned.
-   * Otherwise, it indicates that the memory is ready.
+   * This function checks if the available memory is sufficient based on the
+   * provided memory limit. If the memory limit is set to -1, indicating no
+   * limit, the function considers the memory as ready. Otherwise, it
+   * calculates the available memory by subtracting the current memory usage
+   * from the memory limit. If the available memory is below the threshold of
+   * 200MB, an error message is returned. Otherwise, it indicates that the
+   * memory is ready.
    *
    * @param string | int $memory_limit
    * $memory_limit The memory limit.
    *
    * @return string Returns a message indicating the memory readiness:
-   *   - "Memory is ready" if the memory limit is -1 or the available memory is sufficient.
-   *   - "Error: Not enough memory available" if the available memory is below the threshold.
+   *   - "Memory is ready" if the memory limit is -1 or the available memory is
+   *   sufficient.
+   *   - "Error: Not enough memory available" if the available memory is below
+   *   the threshold.
    */
-  public function check_memory_readiness(string | int $memory_limit): string
-  {
+  public function check_memory_readiness(string|int $memory_limit): string {
     if ($memory_limit === -1) {
       return 'Memory is ready';
     }
@@ -109,7 +152,8 @@ class PhpMemoryController extends ControllerBase
     // The threshold is set to 200MB (1048576 bytes = 1MB)
     if ($available_memory < 1048576 * 200) {
       return 'Error: Not enough memory available';
-    } else {
+    }
+    else {
       return 'Memory is ready';
     }
   }
@@ -117,13 +161,15 @@ class PhpMemoryController extends ControllerBase
   /**
    * Get the size of a variable in memory.
    *
-   * Calculates the approximate size of a variable in memory. The following variable types are supported:
+   * Calculates the approximate size of a variable in memory. The following
+   * variable types are supported:
    * - null: 0 bytes in the heap
    * - bool: 4 bytes in the heap
    * - int: 4 bytes in the heap
    * - float: 8 bytes in the heap
    * - string: Each character occupies 1 byte in the heap
-   * - array: The size is calculated recursively for each key and value in the array
+   * - array: The size is calculated recursively for each key and value in the
+   * array
    * - object: Loading...
    * - callable: (To Do)
    * - resource: (To Do)
@@ -132,8 +178,7 @@ class PhpMemoryController extends ControllerBase
    *
    * @return int The approximate size of the variable in bytes.
    */
-  public function get_var_size(mixed $var): int
-  {
+  public function get_var_size(mixed $var): int {
     // As every variable in PHP is a zval, a zval takes automatically 128 bits (16 bytes) in memory.
     $size = 16;
 
@@ -141,16 +186,20 @@ class PhpMemoryController extends ControllerBase
 
     if ($var_type === 'boolean' || $var_type === 'integer') {
       $size += 4;
-    } elseif ($var_type === 'double') {
+    }
+    elseif ($var_type === 'double') {
       $size += 8;
-    } elseif ($var_type === 'string') {
+    }
+    elseif ($var_type === 'string') {
       $size += strlen($var);
-    } elseif ($var_type === 'array') {
+    }
+    elseif ($var_type === 'array') {
       foreach ($var as $key => $value) {
         $size += get_var_size($key);
         $size += get_var_size($value);
       }
-    } elseif ($var_type === 'object') {
+    }
+    elseif ($var_type === 'object') {
       // Loading...
       $object_vars = get_object_vars($var);
       foreach ($object_vars as $object_var) {
@@ -174,8 +223,7 @@ class PhpMemoryController extends ControllerBase
    *
    * @return array Returns an array containing configuration values
    */
-  public function get_environment_configuration(): array
-  {
+  public function get_environment_configuration(): array {
     $config = $this->config('php_memory_readiness_checker.settings');
     $configurations = $config->get('settings_list');
 
@@ -183,6 +231,18 @@ class PhpMemoryController extends ControllerBase
       $configurations[$key] = ini_get($key);
     }
     return $configurations;
+  }
+
+  private function getModulesSize() {
+    //    return \Drupal::service('module_handler')->loadAll();
+    //    return \Drupal::service('module_handler')->getModuleList();
+    $list_of_modules = \Drupal::service('module_handler')
+      ->getModuleDirectories();
+    $total_size = 0;
+    foreach ($list_of_modules as $module_name => $path) {
+      $total_size += $this->getDirectorySize($path);
+    }
+    return $total_size;
   }
 
 }
