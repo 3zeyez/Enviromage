58b6f6c19060d2df9cc21d99488026eabb897b07
aziz-712 <a7med3bassi.400@gmail.com>
2023-06-27 15:34:12 +0100

    Retrieve Lock File Operations && Package Operations: from `composer update --dry-run --profile` command's output

--- src/Controller/PhpMemoryController.php
+++ src/Controller/PhpMemoryController.php
@@ -19,6 +19,9 @@ use Drupal\Core\Asset\AttachedAssetsInterface;
 use InvalidArgumentException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Render\Markup;
 
 /**
  * Defines phpController class.
@@ -32,12 +35,13 @@ class PhpMemoryController extends ControllerBase {
    * @return array
    *   Return markup array.
    */
-  public function content(): array {
+  public function content(Request $request): array {
+    $this->get_update_info_about_enabled_modules();
     return [
       '#theme' => 'php_memory_readiness_checker',
       '#environment_configuration' => $this->get_environment_configuration(),
-      '#modules_size' => $this->human_filesize($this->getModulesSize()[0]['total_size']),
-      '#each_module' => $this->getModulesSize()[1],
+      '#modules_size' => $this->human_filesize($this->getModulesSize($request)[0]['total_size']),
+      '#each_module' => $this->getModulesSize($request)[1],
     ];
     //    return [
     //      '#markup' => $this->t(implode(', ', $this->listModules())),
@@ -246,7 +250,7 @@ class PhpMemoryController extends ControllerBase {
     return $retrieved_configurations;
   }
 
-  private function getModulesSize() {
+  private function getModulesSize(Request $request) {
     //    return \Drupal::service('module_handler')->loadAll();
     //    return \Drupal::service('module_handler')->getModuleList();
     $list_of_modules = \Drupal::service('module_handler')
@@ -255,8 +259,14 @@ class PhpMemoryController extends ControllerBase {
               1 => []];
     foreach ($list_of_modules as $module_name => $path) {
 //      if ($this->is_module_has_update($module_name)) {
-        $module_name = \Drupal::service('module_handler')->getName($module_name);
-        $module_size = $this->getDirectorySize($path);
+//        $module_name = \Drupal::service('module_handler')->getName($module_name);
+//        echo "<pre>";
+//        print_r(\Drupal::service('module_handler')->getModule($module_name));
+//        echo "</pre>";
+//        $module_size = $this->getDirectorySize($path);
+      /** @var Request $request */
+//      $module_size = $this->getMemoryUsage($request, $module_name);
+       $module_size = $this->my_module_measure_memory_usage();
         $array[1][$module_name] = $this->human_filesize($module_size);
         $array[0]['total_size'] += $module_size;
 //          $files = \Drupal::service('file_system')->scanDirectory($path, '.*');
@@ -283,31 +293,178 @@ class PhpMemoryController extends ControllerBase {
     return $current_version < $latest_version;
   }
 
+  public function getMemoryUsage(Request $request, string $module_name)
+  {
+    $module = $request->get($module_name);
+
+    return memory_get_peak_usage(true);
+
+//    $response = new Response();
+//    $response->setContent(json_encode([
+//      'module' => $module,
+//      'memoryUsage' => $memoryUsage,
+//    ]));
+
+//    return [
+//      'module' => $module,
+//      'memoryUsage' => $memoryUsage,
+//    ];
+//    return $response;
+  }
+
+
+  private function my_module_measure_memory_usage() {
+    $memoryUsage = memory_get_usage(true);
+    $formattedMemoryUsage = Markup::create('<pre>' . format_size($memoryUsage) . '</pre>');
+    dpm($formattedMemoryUsage, 'Memory Usage');
+    return $memoryUsage;
+  }
+
+//  private function get_update_info_about_enabled_modules() {
+////    $available = update_get_available();
+////    $module_info = update_calculate_project_data($available);
+////    foreach ($module_info as $module => $info) {
+////      echo "<h1>$module</h1>";
+////      echo "<pre>"; print_r($info); echo "</pre>";
+////    }
+//
+//    // Your custom module code.
+//
+////    $output = NULL;
+////    $result_code = NULL;
+////    $return_value = exec('ddev exec ls');
+////    // Display the output.
+////    echo gettype($output) . "</br>";
+////    if ($return_value === '') echo '$return_value is empty string!';
+////    echo "<pre>"; print_r($output); echo "</pre>";
+////
+////    // Run the Composer command.
+////    $return_value = exec('ddev exec composer update --dry-run --profile',
+////                  $output,
+////                    $result_code);
+////
+////    // Display the output.
+////    echo gettype($return_value) . "</br>";
+////    if ($return_value === '') echo '$return_value is empty string!';
+////    echo "<pre>"; print_r($output); echo "</pre>";
+//    // Run the Composer command.
+////    $command = 'ddev exec composer update --dry-run --profile';
+////    $command = 'composer update --dry-run --profile';
+////    $command = 'echo foo';
+////    $command = 'cd .. && composer update --dry-run --profile';
+////    $command = 'cd .. && ls -la';
+////    $command = 'cd .. && composer -V';
+//    $command = 'cd .. && composer update --dry-run';
+//    $output = NULL;
+//    $result_code = NULL;
+//    $return_value = exec($command, $output, $result_code);
+//
+//    // Display the output.
+//    echo gettype($return_value) . "</br>";
+//    if ($return_value === '') echo '$return_value is empty string!</br>';
+//    else echo $return_value . "</br>";
+//    if ($result_code == 0) echo "it's a zero</br>";
+//    echo "$result_code</br>";
+//    echo "<pre>"; print_r($output); echo "</pre>";
+//  }
+
   private function get_update_info_about_enabled_modules() {
-    $command = 'cd .. && composer update --dry-run --profile';
-    $output = NULL;
-    $result_code = NULL;
-    $return_value = exec($command, $output, $result_code);
+    $result = $this->run_composer_command();
 
-    // Display the output.
-    echo gettype($return_value) . "</br>";  // check if exec is working fine or not
-    if ($return_value === '') echo '$return_value is empty string!</br>';
-    else echo $return_value . "</br>";
+    if (is_null($result)) {
+      \Drupal::messenger()->addError('Composer command could not run!');
+      exit(1);
+    } elseif (is_array($result)) {
+      $output = $result['output'];
+      $errorOutput = $result['errorOutput'];
+    } elseif (is_string($result)) {
+      $output = $result;
+    }
+    unset($result);
+    if (isset($output)){
+      $lines = explode(PHP_EOL, $output);
+      array_pop($lines);
+      echo "</br></br></br></br>";
+      echo "<pre>"; print_r($lines); echo "</pre>";
 
-    if ($result_code == 0) echo "Zero is returned, the command works fine.</br>";
-    else echo "$result_code</br>";
-    echo "<pre>"; print_r($output); echo "</pre>";
+      $total_memory = 0.0;
+      foreach ($lines as $line) {
+        $closingBracketPos = strpos($line, ']');
+        $substring = substr($line, 1, $closingBracketPos - 1);
+
+        $memory = (float) $substring;
+        $total_memory += $memory;
+
+        $restOfLine1 = trim(substr($line, strlen($substring) + 2));
+
+        // Lock file operations:
+        $beginOfLine = substr($restOfLine1, 0, strlen('Lock file operations:'));
+        if ($beginOfLine === 'Lock file operations:') {
+          echo "Lock file operations:";
+          echo "</br>";
+
+          // Installs
+          $restOfLine2 = trim(substr($restOfLine1, strlen($beginOfLine)));
+          $numberOfInstalls = (int) $restOfLine2;
+          echo "We have $numberOfInstalls Installs.";
+          echo "</br>";
+
+          // Updates
+          $restOfLine2 = substr($restOfLine2, strlen('0 installs,'));
+          $numberOfUpdates = (int) $restOfLine2;
+          echo "We have $numberOfUpdates Updates.";
+          echo "</br>";
+
+          // Removes
+          $restOfLine2 = substr($restOfLine2, strlen('10 updates,'));
+          $numberOfRemoves = (int) $restOfLine2;
+          echo "We have $numberOfRemoves Removes.";
+          echo "</br>";
+        }
+
+        unset($beginOfLine); // end of Lock file operations
+
+        // Package operations:
+        $beginOfLine = substr($restOfLine1, 0, strlen('Package operations:'));
+        if ($beginOfLine === 'Package operations:') {
+          echo "Package operations:";
+          echo "</br>";
+
+          // Installs
+          $restOfLine2 = trim(substr($restOfLine1, strlen($beginOfLine)));
+          $numberOfInstalls = (int) $restOfLine2;
+          echo "We have $numberOfInstalls Installs.";
+          echo "</br>";
+
+          // Updates
+          $restOfLine2 = substr($restOfLine2, strlen('0 installs,'));
+          $numberOfUpdates = (int) $restOfLine2;
+          echo "We have $numberOfUpdates Updates.";
+          echo "</br>";
+
+          // Removes
+          $restOfLine2 = substr($restOfLine2, strlen('10 updates,'));
+          $numberOfRemoves = (int) $restOfLine2;
+          echo "We have $numberOfRemoves Removes.";
+          echo "</br>";
+        }
+      }
+
+      $avg_memory_usage = $total_memory / count($lines);
+      echo "Average memory usage equals to: $avg_memory_usage";
+      echo "</br>";
+    }
   }
 
   // Your custom module code.
-  private function run_composer_command() {
+  private function run_composer_command(): string | array | NULL {
     // Run the shell command within the DDEV environment.
     $descriptors = [
       1 => ['pipe', 'w'], // Capture standard output
       2 => ['pipe', 'w'], // Capture standard error output
     ];
-    $process = proc_open('cd .. && composer update --dry-run --profile',
-      $descriptors, $pipes);
+    $process = proc_open('composer update --dry-run --profile',
+      $descriptors, $pipes, '/var/www/html');
 
     if (is_resource($process)) {
       // Get the standard output
@@ -323,17 +480,16 @@ class PhpMemoryController extends ControllerBase {
       // Close the process
       proc_close($process);
 
-      // Display the output.
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-      echo "<pre>$output</pre></br>Hi! I am Aziz.";
-
-      // Display the error output.
-      echo "<pre>Error Output: $errorOutput</pre>";
+      if ($output == '') {
+        if ($errorOutput == '') {
+          return '';
+        } else {
+          return $errorOutput;
+        }
+      } else {
+        return ['output' => $output, 'errorOutput' => $errorOutput];
+      }
     }
+    return NULL;
   }
 }
