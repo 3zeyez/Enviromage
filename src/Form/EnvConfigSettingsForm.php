12a4c8fd48d5524da33c742392d9396c797b8419
aziz-712 <a7med3bassi.400@gmail.com>
2023-07-13 16:58:55 +0100

    Add modules config: Allow user to choose the package he wants to retrieve its memory size

--- src/Form/EnvConfigSettingsForm.php
+++ src/Form/EnvConfigSettingsForm.php
@@ -50,6 +50,21 @@ class EnvConfigSettingsForm extends ConfigFormBase {
                       your site performance during an update.'),
     ];
 
+    $moduleDirectories = \Drupal::service('module_handler')->getModuleDirectories();
+    $moduleNames = [];
+    foreach ($moduleDirectories as $moduleName => $path) {
+      unset($path);
+      $moduleNames[$moduleName] = $moduleName;
+    }
+
+    $form['modules'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Choose which modules to display'),
+      '#default_value' => $config->get('modules_list'),
+      '#options' => $moduleNames,
+      '#description' => $this->t('We could display each module separately.'),
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -60,13 +75,26 @@ class EnvConfigSettingsForm extends ConfigFormBase {
     $selected_settings = array_filter($form_state->getValue('env_conf'));
 
     if (count($selected_settings) == 0) {
-      \Drupal::messenger()->addWarning(t("One item should be selected at least!
+      \Drupal::messenger()->addWarning(t("Environment Configuration : One item should be selected at least!
       We will not make any changes."));
     }
 
     if (count($selected_settings) != 0) {
       $this->config('php_memory_readiness_checker.settings')
         ->set('settings_list', $selected_settings)
+        ->save();
+    }
+
+    $selected_modules = array_filter($form_state->getValue('modules'));
+
+    if (count($selected_modules) == 0) {
+      \Drupal::messenger()->addWarning(t("Modules Size: One item should be selected at least!
+      We will not make any changes."));
+    }
+
+    if (count($selected_modules) != 0) {
+      $this->config('php_memory_readiness_checker.settings')
+        ->set('modules_list', $selected_modules)
         ->save();
     }
 
