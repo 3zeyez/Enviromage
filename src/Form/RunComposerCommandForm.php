dc32bc842189b84ed02ea521e634123071b17592
aziz-712 <a7med3bassi.400@gmail.com>
2023-08-01 17:32:58 +0100

    Customize Composer Command using a form

--- src/Form/RunComposerCommandForm.php
+++ src/Form/RunComposerCommandForm.php
@@ -53,15 +53,26 @@ class RunComposerCommandForm extends FormBase {
   /**
    * {@inheritdoc}
    */
+  protected function getEditable() {
+    return [
+      'enviromage.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('enviromage.settings');
 
+    $command = $config->get('composer_command');
+
     $form['message1'] = [
       '#type' => 'markup',
-      '#markup' => '<p>Run the following command:
-                    <code>composer update --dry-run --profile</code>.
+      '#markup' => "<p>Run the following command:
+                    <code>$command</code>.
                     This command simulate a composer update without applaying it.
-                    Also, it profiles memory and time usage.</p>',
+                    Also, it profiles memory and time usage.</p>",
     ];
 
     $form['customize_command'] = [
@@ -78,6 +89,7 @@ class RunComposerCommandForm extends FormBase {
 
     $moduleDirectories = \Drupal::service('module_handler')->getModuleDirectories();
     $moduleNames = [];
+    $moduleNames[''] = '-- all Drupal Modules --';
     foreach ($moduleDirectories as $moduleName => $path) {
       unset($path);
       $moduleNames[$moduleName] = $moduleName;
@@ -89,6 +101,11 @@ class RunComposerCommandForm extends FormBase {
       '#description' => t('choose just one'),
       '#options' => $moduleNames,
       '#default_value' => $config->get('modules_list'),
+    ];
+
+    $form['customize_command']['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('Customize Command'),
     ];
 
     $form['message2'] = [
@@ -113,31 +130,10 @@ class RunComposerCommandForm extends FormBase {
    * Submit handler for PHP benchmark AJAX.
    */
   public function runComposerCommand(array &$form, FormStateInterface $form_state): AjaxResponse {
-    $version_constraint = $form_state->getValue('version_constraint');
-    $semver = new Semver($version_constraint);
-
-    $versionParser = new VersionParser();
-
-    try {
-      // The parseConstraints() method will throw an exception if the version constraint is invalid.
-      $versionParser->parseConstraints($version_constraint);
-      // If the version constraint is valid, you can proceed with your code here.
-      // For example, you can install the package using Composer or perform other actions.
-      $version_constraint = 'The version constraint is valid.';
-
-    } catch (\UnexpectedValueException $e) {
-      // Handle the case when the version constraint is invalid.
-      // For example, display an error message or log the error.
-      // You can also check the exception message for more details on why the constraint is invalid.
-      $errorMessage = $e->getMessage();
-      $version_constraint = 'The version constraint is not valid.';
-
-    }
     $result = $this->RunComposerCommand->get_update_info_about_enabled_modules();
     $markup = [
       '#theme' => 'composer_command',
       '#result' => $result,
-      '#version' => $version_constraint,
     ];
     $response = new AjaxResponse();
     $response->addCommand(
@@ -153,5 +149,38 @@ class RunComposerCommandForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $version_constraint = $form_state->getValue('version_constraint');
+    $package = $form_state->getValue('package');
+
+    $versionParser = new VersionParser();
+
+    try {
+      // The parseConstraints() method will throw an exception if the version constraint is invalid.
+      $versionParser->parseConstraints($version_constraint);
+      // If the version constraint is valid, you can proceed with your code here.
+      // For example, you can install the package using Composer or perform other actions.
+      \Drupal::messenger()->addMessage(t('The version constraint is valid.'));
+      $command = "composer update $package:$version_constraint --dry-run --profile";
+
+    } catch (\UnexpectedValueException $e) {
+      // Handle the case when the version constraint is invalid.
+      // For example, display an error message or log the error.
+      // You can also check the exception message for more details on why the constraint is invalid.
+      $errorMessage = $e->getMessage();
+
+      if ($version_constraint === '') {
+        \Drupal::messenger()->addMessage(t('No version constraint is specified'));
+        $command = "composer update $package --dry-run --profile";
+      } else {
+        \Drupal::messenger()->addError(t('The version constraint is not valid.'));
+        $command = '';
+      }
+
+    }
+
+    $this->config('enviromage.settings')
+      ->set('composer_command', $command)
+      ->save();
+
   }
 }
